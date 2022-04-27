e48f977704a029dbeedef000bb6f2e4ca24ad579
Mohamed Mohy <96388937+Mmohyy@users.noreply.github.com>
2022-04-27 18:14:58 +0200

    Add files via upload

---
+++ Algorithmic Toolbox/week3/car_fueling.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int compute_min_refills(int dist, int tank, vector<int> & stops) {
+    // write your code here
+    int Count = 0,i=0,pos=-2,current=0;
+    stops.push_back(dist);
+    while (true)
+    {
+        if (tank + current >= dist)
+           return Count;
+
+
+        if (tank < (stops[i] - current)){
+            pos = i - 1;
+            if (current == stops[pos])
+                return -1;
+
+            Count++;
+            current = stops[i - 1];
+            i--;
+            continue;
+        }
+        i++;
+    }
+    return -1;
+}
+
+
+int main() {
+    int d = 0;
+    cin >> d;
+    int m = 0;
+    cin >> m;
+    int n = 0;
+    cin >> n;
+
+    vector<int> stops(n);
+    for (size_t i = 0; i < n; ++i)
+        cin >> stops.at(i);
+
+    cout << compute_min_refills(d, m, stops) << "\n";
+
+    return 0;
+}
---
+++ Algorithmic Toolbox/week3/different_summands.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> optimal_summands(int n) {
+  vector<int> summands;
+
+    if (n>1){
+        int temp=n;
+        for(int i=1;i<n;i++){
+            if(temp >2*i){
+                summands.push_back(i);
+                temp-=i;
+            }
+            else{
+                summands.push_back(temp);
+                break;
+            }
+        }
+    }
+    else
+        summands.push_back(1);
+  return summands;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> summands = optimal_summands(n);
+  cout << summands.size() << '\n';
+  for (size_t i = 0; i < summands.size(); ++i) {
+    cout << summands[i] << ' ';
+  }
+}
