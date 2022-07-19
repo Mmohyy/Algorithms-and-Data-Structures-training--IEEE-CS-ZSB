ec6fbefe8742264d6ad16c12f25d8b18bb74b943
Mohamed Mohy <96388937+Mmohyy@users.noreply.github.com>
2022-07-19 02:04:33 +0200

    Create binary_search_with_duplicates.cpp

---
+++ Algorithmic Toolbox/week4/binary_search_with_duplicates.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+using std::vector;
+
+int binary_search(const vector<int> &a, int left, int right, int x) {
+
+    if (left<= right){
+        int mid = (left+right)/2;
+        if(x < a[mid]){
+            right = mid-1;
+            return binary_search(a,left,right,x);
+        }
+        else if (x>a[mid]){
+            left = mid+1;
+            return binary_search(a,left,right,x);
+        }
+        else if(x == a[mid]){
+            int pos = mid;
+            while(a[pos-1]==a[mid])
+            pos--;
+            if (pos==mid-1)
+                return mid;
+            return pos;
+        }
+    }
+    else
+        return -1;
+}
+
+int linear_search(const vector<int> &a, int x) {
+  for (size_t i = 0; i < a.size(); ++i) {
+    if (a[i] == x) return i;
+  }
+  return -1;
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); i++) {
+    std::cin >> a[i];
+  }
+  int m;
+  std::cin >> m;
+  vector<int> b(m);
+  for (int i = 0; i < m; ++i) {
+    std::cin >> b[i];
+  }
+  for (int i = 0; i < m; ++i) {
+    std::cout << binary_search(a,0,n-1, b[i]) << ' ';
+  }
+}
