19c14c5b6e2d5e3cf14131b4592328e38669d8a2
Mohamed Mohy <96388937+Mmohyy@users.noreply.github.com>
2022-07-19 02:05:28 +0200

    Add files via upload

---
+++ Algorithmic Toolbox/week4/majority_element.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int get_majority_element(vector<int> &a, int n) {
+
+  //write your code here
+    for(int i=0;i<n;i++){
+        int current =a[i],count =0;
+        for(int j=0;j<n;j++)
+            if(a[j]==current)
+                count++;
+        if(count > (n/2))
+            return 1;
+    }
+    return 0;
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cin >> a[i];
+  }
+  std::cout << get_majority_element(a,n) << '\n';
+}
